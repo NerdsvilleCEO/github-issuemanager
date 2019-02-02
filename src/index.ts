e875422e7242fd5f7bfcf7bc4edbb398b7514589
Josh Santos <josh@omnidapps.com>
2019-02-02 18:47:09 -0500

    Close functionality PoC implemented

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,49 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
+async function close(context: any) {
+  if(context.payload.comment.user.login != context.payload.repository.owner.login) {
+    await context.github.issues.createComment(context.issue({
+      body: `Your user is not allowed to perform this operation`
+    }))
+  } else {
+    const params = {
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      number: context.payload.issue.number
+    }
+    await context.github.issues.update({
+      ...params,
+      state: 'closed'
+    })
+  }
+}
+
+async function run_action(context: any) {
+  // TODO: Think of a better way to handle this, could prototypes be used to handle this?
+  let actions = [
+    {
+    "name": "close",
+    "function": close
+    }
+  ]
+  let phrase = context.payload.comment.body.toLowerCase().split(" ").splice(1).join("_")
+  let ran = false
+  actions.forEach((action) => {
+    if (phrase.includes(action.name)) {
+      console.log(action.function(context))
+      ran = true
+    }
+  })
+  if(!ran) {
+    await context.github.issues.createComment(context.issue({
+      body: `This action is not implemented`
+    }))
+  }
+}
+
 const bot_name = "IssueManagerBot"
 export = (app: Application) => {
  app.on('issues.opened', async (context) => {
-   console.log("Test")
    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
    await context.github.issues.createComment(issueComment)
  })
@@ -13,12 +53,18 @@ export = (app: Application) => {
      return
    }
 
-   const mentions = context.payload.comment.body.includes(bot_name)
+   let comment = context.payload.comment
+   const mentions = comment.body.includes(bot_name)
    if (mentions) {
     app.log.info('mention', bot_name)
-    await context.github.issues.createComment(context.issue({
-      body: `:wave: Hello World! :heart:`
-    }))
+
+    // Figure out way to pull users allowed to use bot actions, would this be 
+    // collaborators w/ write access on repo?
+
+    // Syntax PoC:
+    // - "Add label" untriaged
+    // - "Remove label" untriaged
+    run_action(context)
    }
  })
 }
